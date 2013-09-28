f3584910cb368a54f2278789d6d072d94c15af60
Pavel <php_prog@mail.ru>
2013-09-28 19:53:15 +0400

    FishTypes - create school of fish

---
+++ FishTypes.js
@@ -0,0 +1,50 @@
+/**
+ * Создаём типы рыб (стаи)
+ * @param color              - цвет рыбы
+ * @param stepsBtwnReproduct - должно пройти шагов перед размножением
+ * @param maxNum             - максимум рыб в стае
+ * @param maxStepsInLife     - максимум шагов, после которых рыба умрёт
+ * @param needFood           - хищник (нужна еда для размножения)
+ * @param speed              - скорость рыбы в спокойном состоянии (в возбуждённом х2)
+ * @param rangeOfVisibility  - радиус обзора (как далеко видит рыба)
+ * @constructor
+ */
+function FishTypes(color, stepsBtwnReproduct, maxNum, maxStepsInLife, needFood, speed, rangeOfVisibility) {
+
+    if (typeof stepsBtwnReproduct !== 'number' || stepsBtwnReproduct < 1) {
+        throw new Error("Error with a reproduction");
+    }
+
+    if (typeof maxNum !== 'number' || maxNum < 1) {
+        throw new Error("maxNum must be >= 1");
+    }
+
+    if (typeof maxStepsInLife !== 'number' || maxStepsInLife < 1) {
+        throw new Error("maxStepsInLife must be >= 1");
+    }
+
+    if (needFood !== -1 && needFood !== 1) {
+        throw new Error("Error with needFood");
+    }
+
+    if (typeof speed !== 'number' || speed <= 0) {
+        throw new Error("speed must be > 0");
+    }
+
+    if (typeof rangeOfVisibility !== 'number' || rangeOfVisibility <= 0) {
+        throw new Error("rangeOfVisibility must be > 0");
+    }
+
+
+    this.color = color;
+
+    this.stepsBtwnReproduct = stepsBtwnReproduct;
+
+    this.maxNum         = maxNum;
+    this.maxStepsInLife = maxStepsInLife;
+    this.needFood       = needFood;
+    this.speed          = speed;
+
+    this.rangeOfVisibility = rangeOfVisibility;
+
+}
--- screensaver.js
+++ screensaver.js
@@ -2,9 +2,9 @@
 window.onload = function () {
     var fishType = [];
 
-    fishType.push(new FishTypes('red', 1000, 40, 5000, 1, 1, 100));
-    fishType.push(new FishTypes('blue', 30, 50, 500, -1, 0.7, 35));
-    fishType.push(new FishTypes('green', 150, 50, 1500, -1, 0.9, 50));
+    fishType.push(new FishTypes('red',   1000, 40, 5000,  1,   1, 100));
+    fishType.push(new FishTypes('blue',    30, 50,  500, -1, 0.7,  35));
+    fishType.push(new FishTypes('green',  150, 50, 1500, -1, 0.9,  50));
     fishType.push(new FishTypes('yellow', 300, 15, 1500, -1, 0.99, 50));
 
     console.log(fishType);
